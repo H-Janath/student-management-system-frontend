ebfc9feb85fa527aa70c0c08792c646c523de617
janath <janathhma@gmail.com>
2024-01-19 13:24:32 +0530

    set routings

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,16 +2,16 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { RegisterComponent } from './pages/register/register.component';
-
+import{StudentComponent} from'./pages/student/student.component';
 const routes: Routes = [
-  {
-    path:"",
-    component:HomeComponent
-  },
-  {
-    path:"register",
-    component:RegisterComponent
-  }
-];
+    { path: '', component: HomeComponent },
+    { path: 'register', component: RegisterComponent },
+    {path:'list',component:StudentComponent}
+  ];
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule],
+  })
+  export class AppRoutingModule {}
 
  
