baea00ae385a1d486517c762a5d8526b705fadf6
janath <janathhma@gmail.com>
2024-01-19 23:19:29 +0530

    Add get api for show student

--- src/app/pages/student/student.component.ts
+++ src/app/pages/student/student.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-student',
   templateUrl: './student.component.html',
   styleUrl: './student.component.css'
 })
-export class StudentComponent {
+export class StudentComponent implements OnInit{
 
+  public studentList : any;
+  public http;
+  constructor(private httpClient:HttpClient){
+      this.http = httpClient;
+  }
+  ngOnInit(): void {
+    this.loadStudent();
+  }
+
+  loadStudent(){
+    this.http.get('http://localhost:8080/student')
+    .subscribe((data)=>{
+      console.log(data);
+      this.studentList = data;
+    })
+  }
 }
